f65805d35bd7c64b7dfc1d0e0a64fffe104f2710
jaouherJallouli <jaouherJlouli@gmail.com>
2021-08-24 12:12:14 +0200

    react-stat

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import Person from "./person";
+
+
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			show: false,
+			Person: {
+				fullName: "jlouli Jaouher",
+				bio:
+					"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+				imgSrc: "./me.jpg",
+				profession: " Full-Stack Devlopper",
+			},
+		};
+
+		this.shownow = () => {
+			this.setState({ show: !this.state.show });
+		};
+	}
+	render() {
+		return (
+			<div className='App'>
+       
+				<Button variant='success' onClick={this.shownow}>
+					Show Profile
+				</Button>
+				{this.state.show ? (<Person Person ={this.state.Person}/> ) : ( <h2> Clik to activate the Show function </h2> )}
+        
+			</div>
+		);
+	}
+}
